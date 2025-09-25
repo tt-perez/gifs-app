9651098c8008f1aa41e1c543819c476cdc0c1ee5
Tay <taimarytrimino@gmail.com>
2025-09-25 11:07:34 -0400

    menu options component

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+
+interface MenuOptions {
+label :string;
+subLabel: string;
+route: string;
+icon: string;
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+ menuOptions : MenuOptions[] = [
+  {label: 'Trending',
+    subLabel: 'Popular Gifs',
+    route: '/dashboard/trending',
+    icon: 'fa-solid fa-chart-line'
+  },
+
+    {label: 'Searching',
+    subLabel: 'Search Gifs',
+    route: '/dashboard/search',
+    icon: 'fa-solid fa-magnifying-glass'
+  },
+ ]
+ }
